654f3d5f337cc3d6640d5fca9780aaad06af07b3
Fikremariam Besrat <118328728+fonkamariam@users.noreply.github.com>
2024-04-16 16:28:19 +0300

    Worked on GetUserProfile,UpdateUserProfile

---
+++ Controllers/UserProfile.cs
@@ -0,0 +1,169 @@
+﻿using LiveChat.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Supabase.Interfaces;
+using Supabase;
+
+namespace LiveChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfile : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly Client _supabaseClient;
+
+        public UserProfile(IConfiguration configuration, Client supabaseClient)
+        {
+            _configuration = configuration;
+            _supabaseClient = supabaseClient;
+        }
+
+        
+        
+        [HttpGet("GetUserProfile"),Authorize]
+        public async Task<IActionResult> GetUserProfile()
+        {
+            var phoneNumberClaim = User.Claims.FirstOrDefault(c => c.Type == "PhoneNumber");
+            if (phoneNumberClaim == null)
+            {
+                return BadRequest("Invalid Token");
+            }
+
+            try
+            {
+                var response = await _supabaseClient.From<Userdto>()
+                    .Where(n => n.PhoneNo == phoneNumberClaim.ToString()).Get();
+
+                try
+                {
+                    var hey = response.Models.FirstOrDefault();
+
+                    if (hey == null)
+                    {
+                        return BadRequest("Invalid Token");
+                    }
+
+                   
+
+                    // update Password
+                    try
+                    {
+                        var responseUpdate = await _supabaseClient.From<UserProfiledto>()
+                            .Where(n => n.UserId == hey.Id)
+                            .Single();
+
+                        UserProfileCustom userProfileCustom = new UserProfileCustom
+                        {
+                            Name = responseUpdate.Name,
+                            LastName = responseUpdate.LastName,
+                            UserName = responseUpdate.UserName,
+                            Avatar = responseUpdate.Avatar,
+                            Bio = responseUpdate.Bio
+                        };
+
+                        return Ok(userProfileCustom);
+                    }
+                    catch (Exception)
+                    {
+                        return BadRequest("No Connection for Getting Userprofile");
+                    }
+                }
+                catch (Exception)
+                {
+                    return BadRequest("Problem when querying the database");
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest("No Connection, Please Try again");
+            }
+        }
+
+        [HttpPut("UpdatUserProfile"),Authorize]
+        public async Task<IActionResult> UpdateUserProfile(UserProfileCustom userProfileCustom)
+        {
+            var phoneNumberClaim = User.Claims.FirstOrDefault(c => c.Type == "PhoneNumber");
+            if (phoneNumberClaim == null)
+            {
+                return BadRequest("Invalid Token");
+            }
+
+            try
+            {
+                var response = await _supabaseClient.From<Userdto>()
+                    .Where(n => n.PhoneNo == phoneNumberClaim.ToString()).Get();
+
+                try
+                {
+                    var hey = response.Models.FirstOrDefault();
+
+                    if (hey == null)
+                    {
+                        return BadRequest("Invalid Token");
+                    }
+
+                    var response1 = await _supabaseClient
+                        .From<UserProfiledto>()
+                        .Where(n => n.UserId == hey.Id)
+                        .Single();
+
+                    // Check if the user profile exists
+                    if (response1 == null)
+                    {
+                        return NotFound("User profile not found");
+                    }
+
+                    UserProfiledto userProfiledto = response1 as UserProfiledto;
+
+                    if (!string.IsNullOrEmpty(userProfileCustom.Name))
+                    {
+                        userProfiledto.Name = userProfileCustom.Name;
+                    }
+                    if (!string.IsNullOrEmpty(userProfileCustom.LastName))
+                    {
+                        userProfiledto.LastName = userProfileCustom.LastName;
+                    }
+                    if (!string.IsNullOrEmpty(userProfileCustom.Bio))
+                    {
+                        userProfiledto.Bio = userProfileCustom.Bio;
+                    }
+                    if (!string.IsNullOrEmpty(userProfileCustom.UserName))
+                    {
+                        userProfiledto.Bio = userProfileCustom.UserName;
+                    }
+                    if (!string.IsNullOrEmpty(userProfileCustom.Avatar))
+                    {
+                        userProfiledto.Bio = userProfileCustom.Avatar;
+                    }
+                    // update Profile
+                    try
+                    {
+
+                        var response2 = await _supabaseClient
+                            .From<UserProfiledto>()
+                            .Where(n => n.UserId == hey.Id)
+                            .Update(userProfiledto);
+                        
+
+                        return Ok("Updated UserProfile");
+                    }
+                    catch (Exception)
+                    {
+                        return BadRequest("No Connection for updating Userprofile");
+                    }
+                }
+                catch (Exception)
+                {
+                    return BadRequest("Problem when querying the database");
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest("No Connection, Please Try again");
+            }
+        }
+    }
+}
---
+++ Models/UserProfileCustom.cs
@@ -0,0 +1,17 @@
+﻿namespace LiveChat.Models
+{
+    public class UserProfileCustom
+    {
+        
+        public string? Name { get; set; } = null;
+
+        public string? UserName { get; set; } = null;
+
+        public string? Avatar { get; set; }
+
+        public string? Bio { get; set; }
+
+        public string? LastName { get; set; }
+
+}
+}
